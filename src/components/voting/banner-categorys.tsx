3d47e2559f54f1cb5485daf88cab188f9f50c3ec
Luan Henrique <luanhenriquemiguelalves@gmail.com>
2024-02-26 17:28:24 -0300

    resize oscar

--- src/components/voting/banner-categorys.tsx
+++ src/components/voting/banner-categorys.tsx
@@ -4,10 +4,10 @@ import { SmallCard } from "./small-card";
 
 export function BannerCategorys() {
   return (
-    <div className="m-auto w-11/12 top-40 max-w-3xl relative md:-top-4 ">
+    <div className="m-auto w-11/12 max-w-3xl relative md:-top-4">
       {/* Categorys */}
       <div className="relative z-50">
-        <div className="flex flex-col gap-y-8 items-center absolute -top-6 left-8 z-50 sm:left-20 md:flex-row md:-left-4 md:items-end md:top-14">
+        <div className="flex flex-col gap-4 items-center absolute -top-6 left-4 z-50 sm:left-20 md:flex-row md:-left-4 md:items-end md:top-14">
           <BigCard title={<h1>Pertencimento</h1>} />
           <SmallCard title={<h1>Acolhimento</h1>} />
           <BigCard
@@ -20,18 +20,18 @@ export function BannerCategorys() {
           />
           <SmallCard title={<h1>Inovação</h1>} />
         </div>
-        <div className="flex flex-col gap-4 items-center absolute top-36 right-8 z-50 sm:left-56 sm:top-10 md:flex-row md:top-52 md:left-4 md:items-start lg:top-56">
-          <BigCard
-            title={
-              <h1>
-                Mesquita do <br /> futuro
-              </h1>
-            }
-          />
+        <div className="flex flex-col gap-4 items-center absolute top-4 right-4 z-50 sm:left-0 sm:top-10 md:flex-row md:top-52 md:left-4 md:items-start lg:top-56">
           <SmallCard
             title={
               <h1>
                 Transformação <br /> social
+              </h1>
+            }
+          />
+          <BigCard
+            title={
+              <h1>
+                Mesquita do <br /> futuro
               </h1>
             }
           />
@@ -46,13 +46,13 @@ export function BannerCategorys() {
       </div>
 
       {/* Oscar e Bg degradê */}
-      <div className="flex relative sm:top-0 md:top-4 md:h-[340px] lg:h-[400px] ">
-        <div className="w-full max-w-4xl z-30 m-auto h-[530px] bg-gradient-to-r from-amber-800 to-amber-400 rounded-[35px] md:h-[230px] lg:h-[280px]"></div>
+      <div className="flex relative sm:top-0 md:top-4 h-[520px] md:h-[340px] lg:h-[400px]">
+        <div className="w-full max-w-4xl z-30 m-auto h-[450px] bg-gradient-to-r from-amber-800 to-amber-400 rounded-[35px] md:h-[230px] lg:h-[280px]"></div>
         <img
           src={Oscar}
           alt="Boneco do Oscar"
           id="oscar-voting"
-          className="-top-52 right-2 z-50 absolute w-48 sm:-top-4 sm:right-4 md:-top-32 md:-right-4 md:w-64 "
+          className="z-50 hidden absolute sm:block w-44 -right-4 -top-4 sm:right-2 sm:w-64 md:w-52 md:-top-20 md:-right-4 lg:w-56"
         />
       </div>
     </div>
