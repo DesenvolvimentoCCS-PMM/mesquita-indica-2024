35020dc43a993aa603f6982128675ff3f9b9d6aa
luan henrique <luanhenrique.devweb@gmail.com>
2024-02-27 19:53:10 -0300

    feat: alterando texto do botao e adiciondo link no banner

--- src/components/voting/banner-categorys.tsx
+++ src/components/voting/banner-categorys.tsx
@@ -6,7 +6,10 @@ import star2 from "../../assets/voting/estrela2.svg";
 
 export function BannerCategorys() {
   return (
-    <div className="flex relative p-1 mt-48 max-w-[340px] m-auto sm:max-w-none w-11/12 md:mt-20 justify-center lg:mt-32">
+    <a
+      href="#opcoes"
+      className="flex relative p-1 mt-48 max-w-[340px] m-auto sm:max-w-none w-11/12 md:mt-20 justify-center cursor-default lg:mt-32"
+    >
       <div className="w-full max-w-[840px] z-40 relative p-4 h-[510px] bg-gradient-to-r from-amber-800 to-amber-400 rounded-[35px] mdx:flex mdx:justify-start mdx:items-center mdx:h-[280px] lg:h-[300px]">
         <div className="z-40 flex gap-4 justify-center items-end sm:justify-start  mdx:flex-col mdx:items-start mdx:justify-center md:ml-10">
           <div className="space-y-8 flex flex-col items-center relative z-40 -top-10 mdx:flex-row mdx:top-2 mdx:gap-x-4 lg:-left-8">
@@ -80,7 +83,7 @@ export function BannerCategorys() {
           <img
             src={star}
             alt="Estrelas douradas"
-            className="w-20 absolute -top-14 left-14 sm:left-16 sm:w-28 lg:left-12"
+            className="w-20 absolute -top-14 left-10 sm:left-16 sm:w-28 lg:left-12"
           />
         </div>
 
@@ -90,6 +93,6 @@ export function BannerCategorys() {
           className="hidden absolute sm:right-36 sm:bottom-10 sm:w-28 z-50 lg:block lg:right-56 lg:bottom-14"
         />
       </div>
-    </div>
+    </a>
   );
 }
