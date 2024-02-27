cfe5cde2e8a57ef2a6dd4174a28af62c946af035
luan henrique <luanhenrique.devweb@gmail.com>
2024-02-27 10:24:44 -0300

    style: alinhando texto da categoria

--- src/components/voting/slide-category.tsx
+++ src/components/voting/slide-category.tsx
@@ -78,8 +78,8 @@ const CategorySlide: React.FC<CategorySlideProps> = ({
 
   return (
     <div className="mx-6 sm:mx-10">
-      <div className="grid place-content-center text-center  mb-8">
-        <span className="text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-r from-amber-100 to-amber-300 sm:text-3xl">
+      <div className="grid place-content-center text-center mb-8">
+        <span className="text-lg relative -bottom-2 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-amber-100 to-amber-300 sm:text-xl sm:mb-0 md:text-3xl">
           Categoria
         </span>
         <h2 className="text-3xl uppercase py-2 text-center transition-all font-bold bg-clip-text text-transparent bg-gradient-to-r from-amber-700 to-amber-400 md:text-5xl">
